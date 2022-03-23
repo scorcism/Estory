a6f543899e549c9e542f0177392e4824d4ec7f8b
scorcism <abhishekpathak1720@gmail.com>
2022-03-24 01:45:10 +0530

    Image Gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import ImageGrid from '../components/ImageGrid'
+import UploadForm from '../components/UploadForm'
 
 function Gallery() {
   return (
     <>
-    <div className="container">
-
-    <h3 className="text-center mt-4 mb-2">Image Gallery</h3>
-    <p className='text-center'>Share your Images here</p>
-    </div>
+      <div className="container">
+        <h3 className="text-center mt-4 mb-2">Image Gallery</h3>
+        <p className='text-center'>Share your Images here</p>
+        <UploadForm/>
+        <div className="mt-2 mb-2">
+          <ImageGrid/>
+        </div>
+      </div>
     </>
   )
 }
