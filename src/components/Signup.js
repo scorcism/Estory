0b6291bf14f409b2614c91e047d37802990e16a5
scorcism <abhishekpathak1720@gmail.com>
2022-03-25 12:44:37 +0530

    edit unused states

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { Link, Navigate, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import userContext from "../context/userContext"
 import { db } from '../firebase-config';
 import { collection, addDoc } from 'firebase/firestore'
@@ -51,17 +51,17 @@ function Signup() {
                             {error}
                         </div>}
                         <div className="mb-3">
-                            <label htmlFor="name" className="form-label">Name </label>
+                            <label htmlhtmlFor="name" className="form-label">Name </label>
                             <input name='name' onChange={handleChange} value={cred.name} type="text" className="form-control" id="name" aria-describedby="name" />
                             <div id="name" className="form-text">Hello {cred.name}</div>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                            <label htmlhtmlFor="exampleInputEmail1" className="form-label">Email address</label>
                             <input name='email'  onChange={handleChange} value={cred.email} type="email" className="form-control" id="email" aria-describedby="emailHelp" />
                             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div className="mb-3">
-                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                            <label htmlhtmlFor="exampleInputPassword1" className="form-label">Password</label>
                             <input name='password' onChange={handleChange} value={cred.password} type="password" className="form-control" id="password" />
                         </div>
                         <div className="mb-3">
