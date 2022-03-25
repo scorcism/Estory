f9e6538bbbef58a1eb955cee64ff8b152afec6e1
scorcism <abhishekpathak1720@gmail.com>
2022-03-25 12:45:23 +0530

    img ref

--- src/components/SingleImage.js
+++ src/components/SingleImage.js
@@ -6,11 +6,11 @@ function SingleImage({ img }) {
     <>
 
         <div className="col-6 col-md-4 my-3">
-            <div class="card" style={{width: "18rem;"}}>
-              <img style={{objectFit:"contain"}} width="300" height="300" src={img.url} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <button className=' ms-3 me-3 btn btn-outline-info'><a style={{textDecoration:"none", color:"black",fontWeight:"400"}} href={`https://www.instagram.com/${img.insta}`} targe="_blank">Owner</a></button>
-                <button className=' ms-3 me-3 btn btn-outline-secondary'><a style={{textDecoration:"none", color:"black",fontWeight:"400"}} href={img.url} target="_blank">Full Image</a></button>
+            <div className="card" style={{width: "18rem;"}}>
+              <img style={{objectFit:"contain"}} width="300" height="300" src={img.url} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <button className=' ms-3 me-3 btn btn-outline-info'><a style={{textDecoration:"none", color:"black",fontWeight:"400"}} href={`https://www.instagram.com/${img.insta}`} rel="noreferrer" targe="_blank">Owner</a></button>
+                <button className=' ms-3 me-3 btn btn-outline-secondary'><a style={{textDecoration:"none", color:"black",fontWeight:"400"}} href={img.url} rel="noreferrer" target="_blank">Full Image</a></button>
             </div>
         </div>
       </div>
